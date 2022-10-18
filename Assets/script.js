2a9c5030f2acc4b7b9124f0138dc03c5cec1896b
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-10-18 09:09:33 -0500

    fixed colors

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
   //display current day & time.
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm"));
 
   //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
@@ -13,15 +13,15 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
         localStorage.setItem(time, text);
     })
   //load any saved data from LocalStorage - do this for each hour created.
-    $("#hour9.description").val(localStorage.getItem("hour9"));
-    $("#hour10.description").val(localStorage.getItem("hour10"));
-    $("#hour11.description").val(localStorage.getItem("hour11"));
-    $("#hour12.description").val(localStorage.getItem("hour12"));
-    $("#hour13.description").val(localStorage.getItem("hour13"));
-    $("#hour14.description").val(localStorage.getItem("hour14"));
-    $("#hour15.description").val(localStorage.getItem("hour15"));
-    $("#hour16.description").val(localStorage.getItem("hour16"));
-    $("#hour17.description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
     function hourTracker() {
@@ -30,16 +30,16 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
 
       // loop over time blocks
         $(".time-block").each(function () {
-            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( hourBlock, currentHour)
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
 
           //check if we've moved past this time
-            if (hourBlock < currentHour) {
+            if (blockHour < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
             }
-            else if (hourBlock === currentHour) {
+            else if (blockHour === currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
                 $(this).removeClass("future");
